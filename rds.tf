c3ac9dc1e9333e5aabd68fd92a0771f2108029e8
koh-yoshimoto <yoshimoto.koshiro@gmail.com>
2023-11-08 21:58:00 +0900

    add db option group

--- rds.tf
+++ rds.tf
@@ -16,3 +16,11 @@ resource "aws_db_parameter_group" "mysql_standalone_parametergroup" {
   }
 }
 
+# ---------------------------
+# RDS Option Group
+# ---------------------------
+resource "aws_db_option_group" "mysql_standalone_optiongroup" {
+  name                 = "${var.project}-${var.environment}-mysql-standalone-optiongroup"
+  engine_name          = "mysql"
+  major_engine_version = "8.0"
+}
